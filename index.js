6bebd19b171fb3b78637263cc5f8d6b014ed47c8
Tech Youths <arsalanmohd237@gmail.com>
2021-06-17 11:46:12 +0530

    event listener to form

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 const express = require('express')
 const app = express()
 const server = require('http').createServer(app)
+const { Server } = require('socket.io')
+const io = new Server(server)
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
 	res.render('index')
+})
+
+io.on('connection', (socket) => {
+	console.log('User Connected')
+
+	socket.on('disconnect', () => {
+		console.log('User Disconnected')
+	})
+
+	socket.on('send message', (msg) => {
+		console.log('Message is: ' + msg)
+	})
 })
 
 server.listen(3000, () => {
